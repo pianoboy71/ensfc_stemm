6dc64fd07bf64d97980671ee2378b3796faa23c6
Jack Gallant <concise_sparsity287626@outlook.com>
2025-01-10 20:57:12 +0000

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='ensfc-stemm',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         "sympy"
-    ]
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
