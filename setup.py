c2a37b01c64cdee654df32a9e9f61f2736009bcb
Jack Gallant <concise_sparsity287626@outlook.com>
2025-01-10 20:41:16 +0000

    corrected bumpversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ensfc-stemm',
-    __version__='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         "sympy"
